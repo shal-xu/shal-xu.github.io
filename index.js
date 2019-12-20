5b62730b3a12edf340fd806201537c4c178ad77d
Shal <shal.xu@economicmodeling.com>
2019-12-20 10:31:21 -0800

    save all the images locally

--- index.js
+++ index.js
@@ -24,47 +24,39 @@ $(window).on('load', function() {
   const menuItems = [
     {
       name: 'Wonton Noodle Soup with Bok Choy',
-      image:
-        'https://seonkyounglongest.com/wp-content/uploads/2018/12/Wonton-Noodle-Soup-2.jpg'
+      image: 'images/wonton-noodle-soup.jpg'
     },
     {
       name: 'Century Egg, Pork and Asparagus Porridge',
-      image:
-        'https://tasteasianfood.com/wp-content/uploads/2015/12/porridge-700x450-.jpg'
+      image: 'images/porridge.jpg'
     },
     {
       name: 'Fried Breadsticks',
-      image: 'https://i.ytimg.com/vi/wbAEtf3gVYo/maxresdefault.jpg'
+      image: 'images/fried-breadsticks.jpg'
     },
     {
       name: 'Spiced Sunflower Seeds',
-      image:
-        'https://image.made-in-china.com/2f0j00qNjaWgJwknuU/601-Sunflower-Seeds-New-Crop.jpg'
+      image: 'images/sunflower-seeds.jpg'
     },
     {
       name: ' Asian Pancakes',
-      image:
-        'https://i.pinimg.com/originals/82/b3/e1/82b3e1cbaadac3c85c2a7bba5003ed96.jpg'
+      image: 'images/asian-pancakes.jpg'
     },
     {
       name: ' Soymilk',
-      image:
-        'https://previews.123rf.com/images/winnond/winnond1207/winnond120700005/14260943-soy-milk-with-raw-soy-beans-background.jpg'
+      image: 'images/soymilk.jpg'
     },
     {
       name: 'Homemade Apple Sauce',
-      image:
-        'https://www.seriouseats.com/recipes/images/2014/12/20141203-latke-vicky-wasik-31-1500x1125.jpg'
+      image: 'images/apple-sauce.jpg'
     },
     {
       name: '  Taro Bun',
-      image:
-        'https://www.unfamiliarchina.com/images/dim-sum/sweet-yam-taro-buns.jpg'
+      image: 'images/taro-buns.jpg'
     },
     {
       name: '  Egg Tarts',
-      image:
-        'https://media.eggs.ca/assets/RecipePhotos/_resampled/FillWyIxMjgwIiwiNzIwIl0/Traditional-egg-tart-CMS.jpg'
+      image: 'images/egg-tarts.jpg'
     }
   ];
 
