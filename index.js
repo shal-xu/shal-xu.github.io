5a334e0fa981638b9754b0463bdd6bbf361ae796
Shal <shal.xu@economicmodeling.com>
2019-12-19 16:44:57 -0800

    Reached a presentable point

--- index.js
+++ index.js
@@ -1,27 +1,25 @@
 $(window).on('load', function() {
-  $('nav > button').each((index, element) =>
-    $(element).click(() => {
-      $('.highlight').css('left', `calc(${index * 33 + 16.5}% - 67px)`);
-      $(element).css(
+  const setActiveTab = index => {
+    $('.highlight').css('left', `calc(${index * 33 + 16.5}% - 67px)`);
+    $('nav > button')
+      .not()
+      .css('color', 'white');
+    $('nav>button')
+      .eq(index)
+      .css(
         'color',
         `rgba(${255 - index * 10},${208 - 69 * index},${18 - 6 * index},1)`
       );
-      $('nav > button')
-        .not(element)
-        .css('color', 'white');
-      $('.page').css('transform', `translate(${-1 * index}00%)`);
-    })
+    $('.page').css('transform', `translate(${-1 * index}00%)`);
+  };
+
+  $('nav > button').each((index, element) =>
+    $(element).click(() => setActiveTab(index))
   );
 
-  $('#rsvp').click(() => {
-    $('.highlight').css('left', `calc(82.5% - 67px)`);
-    $(nav > button)[2].css(
-      'color',
-      `rgba(${255 - 2 * 10},${208 - 69 * 2},${18 - 6 * 2},1)`
-    );
-    $('nav > button')[0].css('color', 'white');
-    $('.page').css('transform', `translate(${-1 * 2}00%)`);
-  });
+  $('#rsvp').click(() => setActiveTab(2));
+
+  setActiveTab(0);
 
   const menuItems = [
     {
